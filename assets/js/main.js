881b4ad5b4477cec0f324016e106efb452567ac8
m1lkyway123 <124919438+m1lkyway123@users.noreply.github.com>
2023-12-27 12:05:12 +0200

    added swipers

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,3 +9,23 @@ const swiper = new Swiper(".header__slider-items", {
   },
   effect: "fade",
 });
+
+const collectionSwiper = new Swiper(".collection__swiper", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".collection-button-next",
+    prevEl: ".collection-button-prev",
+  },
+});
+
+const releasesSwiper = new Swiper(".releases__swiper", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".releases-button-next",
+    prevEl: ".releases-button-prev",
+  },
+});
